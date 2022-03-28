aa34d0d6e5ad3fe5e63ea5517e732254ddea628d
hubert <kuchhubert@gmail.com>
2022-03-28 20:49:33 +0200

    Create avocado repository definitions

--- lib/index.php
+++ lib/index.php
@@ -173,13 +173,26 @@ class AvocadoORMModel extends AvocadoORMSettings {
     }
 }
 
-interface AvocadoRepositoryMethods {
+interface AvocadoRepositoryActions {
     public function findMany(array $criteria);
     public function findOne(array $criteria);
     public function findOneById(string|int $id);
+
+    public function updateMany(array $updateCriteria, array $criteria);
+    public function updateOne(array $updateCriteria, array $criteria);
+    public function updateOneById(array $updateCriteria, string|int  $id);
+
+    public function deleteMany(array $criteria);
+    public function deleteOneById(string|int $id);
+
+     public function save(object $entity);
+     public function saveMany(array $entities);
+
+     public function truncate();
+     public function renameTo(string $to);
 }
 
-class AvocadoRepository extends AvocadoORMModel implements AvocadoRepositoryMethods {
+class AvocadoRepository extends AvocadoORMModel implements AvocadoRepositoryActions {
     public function __construct($model) {
         parent::__construct($model);
     }
