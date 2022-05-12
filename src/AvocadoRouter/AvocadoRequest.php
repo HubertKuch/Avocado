36373a60259687bad59cd0fe75b5c35360c1fdbd
hubert <kuchhubert@gmail.com>
2022-05-12 08:30:33 +0200

    Support for non-apache servers: `getAllHeaders` function

--- src/AvocadoRouter/AvocadoRequest.php
+++ src/AvocadoRouter/AvocadoRequest.php
@@ -9,6 +9,17 @@ class AvocadoRequest {
     public array $params;
     public array $headers;
     public array $locals;
+    public string $method;
+
+    private function getAllHeaders(): array{
+        $headers = [];
+        foreach ($_SERVER as $name => $value) {
+            if (str_starts_with($name, 'HTTP_')) {
+                $headers[str_replace(' ', '-', ucwords(strtolower(str_replace('_', ' ', substr($name, 5)))))] = $value;
+            }
+        }
+        return $headers;
+    }
 
     public function __construct(array $params = array()) {
         $this->body = $_POST;
