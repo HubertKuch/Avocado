459a131c3c2c14afb98b527b534334a3a264998b
hubert <kuchhubert@gmail.com>
2022-06-05 21:15:25 +0200

    Fix `getAllHeaders` function

--- src/AvocadoRouter/AvocadoRequest.php
+++ src/AvocadoRouter/AvocadoRequest.php
@@ -17,11 +17,17 @@ class AvocadoRequest {
 
     private function getAllHeaders(): array{
         $headers = [];
-        foreach ($_SERVER as $name => $value) {
-            if (str_starts_with($name, 'HTTP_')) {
-                $headers[str_replace(' ', '-', ucwords(strtolower(str_replace('_', ' ', substr($name, 5)))))] = $value;
+
+        if (function_exists('getallheaders')) {
+            $headers = getallheaders();
+        } else {
+            foreach ($_SERVER as $name => $value) {
+                if (str_starts_with($name, 'HTTP_')) {
+                    $headers[str_replace(' ', '-', ucwords(strtolower(str_replace('_', ' ', substr($name, 5)))))] = $value;
+                }
             }
         }
+
         return $headers;
     }
 
