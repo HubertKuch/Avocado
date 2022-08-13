6b0537550f354e7a216e7e5737e70c2e1944e7d9
hubert <kuchhubert@gmail.com>
2022-08-13 09:43:23 +0200

    Create Leaf and Configuration annotations

--- src/AvocadoApplication/Attributes/Configuration.php
+++ src/AvocadoApplication/Attributes/Configuration.php
@@ -3,6 +3,67 @@
 namespace Avocado\AvocadoApplication\Attributes;
 
 use Attribute;
+use Avocado\AvocadoApplication\Exceptions\ClassNotFoundException;
+use Avocado\AvocadoApplication\Exceptions\InvalidResourceException;
+use ReflectionClass;
+use ReflectionException;
 
 #[Attribute(Attribute::TARGET_CLASS)]
-class Configuration {}
+class Configuration {
+    private string $targetClassName;
+    private ReflectionClass $targetReflection;
+
+    /**
+     * @throws ClassNotFoundException
+     */
+    public function __construct(string $targetClassName = "") {
+        if ($targetClassName == "") {
+            return;
+        }
+
+        try {
+            $this->targetClassName = $targetClassName;
+            $this->targetReflection = new ReflectionClass($targetClassName);
+        } catch (ReflectionException) {
+            throw new ClassNotFoundException("Class `$targetClassName` was not found.");
+        }
+    }
+
+    public static function isConfiguration(string $class): bool {
+        try {
+            $reflection = new ReflectionClass($class);
+            $configurationAnnotations = $reflection->getAttributes(Configuration::class);
+
+            return !empty($configurationAnnotations);
+        } catch (ReflectionException) {
+            return false;
+        }
+    }
+
+    /**
+     * @throws InvalidResourceException
+     */
+    public function getInstance(): object {
+        try {
+            return $this->targetReflection->newInstance();
+        } catch (ReflectionException) {
+            throw new InvalidResourceException("Configuration cannot have any constructor parameters, only public constructor without parameters.");
+        }
+    }
+
+    /**
+     * @return Leaf[]
+     * @throws InvalidResourceException
+     */
+    public function getLeafs(Configuration $configuration): array {
+        $methods = $this->targetReflection->getMethods();
+
+        return array_map(
+            fn($method) => Leaf::instance($method, $configuration),
+            array_filter(
+                $methods,
+                fn($method) => !empty($method->getAttributes(Leaf::class))
+            )
+        );
+    }
+}
--- src/AvocadoApplication/Attributes/Leaf.php
+++ src/AvocadoApplication/Attributes/Leaf.php
@@ -3,6 +3,91 @@
 namespace Avocado\AvocadoApplication\Attributes;
 
 use Attribute;
+use Avocado\AvocadoApplication\Exceptions\InvalidResourceException;
+use ReflectionException;
+use ReflectionMethod;
 
 #[Attribute(Attribute::TARGET_METHOD)]
-class Leaf {}
+class Leaf {
+    private string $methodName;
+    private string $leafName;
+    private ReflectionMethod $reflectionMethod;
+    private string $returnType;
+    private object $resourceInstance;
+    private Configuration $configuration;
+
+    public function __construct(?string $name = "") {}
+
+    /**
+     * @throws InvalidResourceException
+     */
+    public function init(ReflectionMethod $method, Configuration $configuration): Leaf {
+        $this->methodName = $method->getName();
+        $this->reflectionMethod = $method;
+        $this->configuration = $configuration;
+        $this->leafName = $this->getLeafName();
+
+        $returnTypes = $method->getReturnType()->getName();
+
+        if (empty($returnTypes)) {
+            throw new InvalidResourceException("Any Leaf must have to have exactly one non-primitive return type.");
+        }
+
+        $this->returnType = $this->reflectionMethod->getReturnType()->getName();
+        try {
+            $this->resourceInstance = $method->invoke($this->configuration->getInstance());
+        } catch (ReflectionException $e) {
+            throw new InvalidResourceException("Any Leaf must have to have exactly one non-primitive return type.");
+        }
+
+        return $this;
+    }
+
+    private function getLeafName(): string {
+        $reflection = $this->getReflectionMethod();
+
+        $leafAttribute = $reflection->getAttributes(Leaf::class);
+        $args = $leafAttribute[0]->getArguments();
+
+        if (empty($args)) {
+            return "";
+        }
+
+        return key_exists('name', $args) ? $args['name'] : $args[0];
+    }
+
+    /**
+     * @throws InvalidResourceException
+     */
+    public static function instance(ReflectionMethod $method, Configuration $configuration): Leaf {
+        return (new Leaf())->init($method, $configuration);
+    }
+
+    public function getMethodName(): string {
+        return $this->methodName;
+    }
+
+    public function setMethodName(string $methodName): void {
+        $this->methodName = $methodName;
+    }
+
+    public function getReflectionMethod(): ReflectionMethod {
+        return $this->reflectionMethod;
+    }
+
+    public function getType(): string {
+        return $this->returnType;
+    }
+
+    public function getName(): string {
+        return $this->leafName;
+    }
+
+    public function setReflectionMethod(ReflectionMethod $reflectionMethod): void {
+        $this->reflectionMethod = $reflectionMethod;
+    }
+
+    public function getResourceInstance(): object {
+        return $this->resourceInstance;
+    }
+}
