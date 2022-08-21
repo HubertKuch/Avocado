7c2cc91c5d3387921c76c85b8fca72c8e908b4f1
hubert <kuchhubert@gmail.com>
2022-08-21 13:32:35 +0200

    MySql connection - get query builder

--- src/DataSource/Drivers/Connection/MySQLConnection.php
+++ src/DataSource/Drivers/Connection/MySQLConnection.php
@@ -2,9 +2,12 @@
 
 namespace Avocado\DataSource\Drivers\Connection;
 
+use Avocado\DataSource\Builder\SQLBuilder;
+use PDO;
+use Avocado\DataSource\Builder\Builder;
+use Avocado\DataSource\Builder\MySQLQueryBuilder;
 use Avocado\DataSource\Database\Statement\MySQLStatement;
 use Avocado\DataSource\Database\Statement\Statement;
-use PDO;
 
 class MySQLConnection implements Connection {
     private ?PDO $pdo;
@@ -13,15 +16,15 @@ class MySQLConnection implements Connection {
         $this->pdo = $pdo;
     }
 
+    public function queryBuilder(): SQLBuilder {
+        return new MySQLQueryBuilder();
+    }
+
     public function prepare(string $sql): Statement {
         return new MySQLStatement($this->pdo, $sql);
     }
 
     public function close(): void {
         $this->pdo = null;
-    }
-
-    public function getPDO(): PDO {
-        return $this->pdo;
     }
 }
