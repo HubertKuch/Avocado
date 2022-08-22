fb5f2a92fbca7cf81c9ee4964f98aebdfa550a68
hubert <kuchhubert@gmail.com>
2022-08-22 14:25:19 +0200

    Refactor actions names and remove updateOne action

--- src/AvocadoORM/Actions/Actions.php
+++ src/AvocadoORM/Actions/Actions.php
@@ -4,14 +4,13 @@ namespace Avocado\AvocadoORM\Actions;
 
 interface   Actions {
     public function findMany(array $criteria);
-    public function findOne(array $criteria);
-    public function findOneById(string|int $id);
+    public function findFirst(array $criteria);
+    public function findById(string|int $id);
     public function findOneToManyRelation(array $findCriteria, ?array $criteria);
     public function paginate(int $limit, int $offset);
 
     public function updateMany(array $updateCriteria, array $criteria);
-    public function updateOne(array $updateCriteria, array $criteria);
-    public function updateOneById(array $updateCriteria, string|int  $id);
+    public function updateById(array $updateCriteria, string|int $id);
 
     public function deleteMany(array $criteria);
     public function deleteOneById(string|int $id);
