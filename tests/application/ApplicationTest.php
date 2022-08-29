9d8fb2d2bedf539ded1a0f1ead995ad24b816fd3
hubert <kuchhubert@gmail.com>
2022-08-29 22:13:32 +0200

    Replace `Application::run` with main application class declared by user

--- tests/application/ApplicationTest.php
+++ tests/application/ApplicationTest.php
@@ -19,7 +19,7 @@ class ApplicationTest extends TestCase {
      * */
     public function testRestGettingControllers(): void {
         $_SERVER['REQUEST_METHOD'] = "GET";
-        Application::run(__DIR__);
+        MockedApplication::init();
 
         self::assertStringContainsString("test", "test");
 
@@ -34,7 +34,7 @@ class ApplicationTest extends TestCase {
      * */
     public function testGettingControllerRoutes(): void {
         $_SERVER['REQUEST_METHOD'] = "GET";
-        Application::run(__DIR__);
+        MockedApplication::init();
 
         $applicationReflection = new ReflectionClass(Application::class);
         $controller = $applicationReflection->getStaticPropertyValue('restControllers')[0];
@@ -55,7 +55,7 @@ class ApplicationTest extends TestCase {
 
         $_SERVER['PHP_SELF'].="/avocado-test";
 
-        Application::run(__DIR__);
+        MockedApplication::init();
 
         self::assertSame('["Get Hello World"]', ob_get_contents());
     }
@@ -68,7 +68,7 @@ class ApplicationTest extends TestCase {
 
         $_SERVER['PHP_SELF'].="/avocado-test";
 
-        Application::run(__DIR__);
+        MockedApplication::init();
 
         self::assertSame('["Post Hello World"]', ob_get_contents());
     }
@@ -81,7 +81,7 @@ class ApplicationTest extends TestCase {
 
         $_SERVER['PHP_SELF'].="/avocado-test";
 
-        Application::run(__DIR__);
+        MockedApplication::init();
 
         self::assertSame('["Delete Hello World"]', ob_get_contents());
     }
@@ -94,7 +94,7 @@ class ApplicationTest extends TestCase {
 
         $_SERVER['PHP_SELF'].="/avocado-test";
 
-        Application::run(__DIR__);
+        MockedApplication::init();
 
         self::assertSame('["Patch Hello World"]', ob_get_contents());
     }
@@ -107,7 +107,7 @@ class ApplicationTest extends TestCase {
 
         $_SERVER['PHP_SELF'].="/avocado-test";
 
-        Application::run(__DIR__);
+        MockedApplication::init();
 
         self::assertSame('["Put Hello World"]', ob_get_contents());
     }
@@ -120,7 +120,7 @@ class ApplicationTest extends TestCase {
 
         $_SERVER['PHP_SELF'].="/avocado-test/array/";
 
-        Application::run(__DIR__);
+        MockedApplication::init();
 
         self::assertSame('["Get Hello World Array"]', ob_get_contents());
     }
@@ -130,7 +130,7 @@ class ApplicationTest extends TestCase {
 
         $_SERVER['PHP_SELF'].="/avocado-test/exception/";
 
-        Application::run(__DIR__);
+        MockedApplication::init();
 
         self::assertSame('{"status":400,"message":"test"}', ob_get_contents());
     }
@@ -140,7 +140,7 @@ class ApplicationTest extends TestCase {
 
         $_SERVER['PHP_SELF'].="/avocado-test/exception/auto-response";
 
-        Application::run(__DIR__);
+        MockedApplication::init();
 
         self::assertSame('{"message":"auto response","status":409}', ob_get_contents());
     }
@@ -150,8 +150,20 @@ class ApplicationTest extends TestCase {
 
         $_SERVER['PHP_SELF'].="/jdskaild";
 
-        Application::run(__DIR__);
+        MockedApplication::init();
 
-        self::assertSame(true, true);
+        self::assertSame('{"message":"Page was not found","status":404}', ob_get_contents());
+    }
+
+    public function testExcludedClasses() {
+        $_SERVER['REQUEST_METHOD'] = "GET";
+
+        MockedApplication::init();
+
+        $ref = new ReflectionClass(Application::class);
+
+        $classes = $ref->getStaticPropertyValue("declaredClasses");
+
+        self::assertFalse(in_array(TestClassToExclude::class, $classes));
     }
 }
