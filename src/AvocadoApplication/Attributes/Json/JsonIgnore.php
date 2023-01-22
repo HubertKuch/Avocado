6ff3621e05aa149e38304c5c70562ccded2d4719
HubertKuch <kuchhubert@gmail.com>
2023-01-22 22:50:02 +0100

    Clean up JsonIgnore class

--- src/AvocadoApplication/Attributes/Json/JsonIgnore.php
+++ src/AvocadoApplication/Attributes/Json/JsonIgnore.php
@@ -10,31 +10,5 @@ use ReflectionProperty;
 #[Attribute(Attribute::TARGET_PROPERTY)]
 class JsonIgnore {
 
-    public function __construct(
-        private ?string $targetProperty = null,
-        private ?string $targetClass = null,
-        private ?ReflectionProperty $reflectionProperty = null
-    ) {}
-
-    public function getReflectionProperty(): ?ReflectionProperty {
-        return $this->reflectionProperty;
-    }
-
-    public function getTargetClass(): ?string {
-        return $this->targetClass;
-    }
-
-    public function getTargetProperty(): ?string {
-        return $this->targetProperty;
-    }
-
-    public static function isAnnotated(string $className, string $propertyName): bool {
-        try {
-            $ref = new ReflectionProperty($className, $propertyName);
-
-            return AnnotationUtils::isAnnotated($ref, self::class);
-        } catch (\Exception) {
-            return false;
-        }
-    }
+    public function __construct() {}
 }
