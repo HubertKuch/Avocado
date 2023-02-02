4197356162c404da0dcbbb4edf6a7ec241c414bc
HubertKuch <kuchhubert@gmail.com>
2023-02-02 06:29:02 +0100

    Removed Avocado views logic

--- src/AvocadoView/AvocadoViewActionException.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace Avocado\AvocadoView;
-
-class AvocadoViewActionException extends \Exception {
-    public function __construct($message, int $code = 0) {
-        parent::__construct($message, $code);
-    }
-}
--- src/AvocadoView/AvocadoViewNotFoundException.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace Avocado\AvocadoView;
-
-class AvocadoViewNotFoundException extends \Exception {
-    public function __construct($message, int $code = 0) {
-        parent::__construct($message, $code);
-    }
-}
