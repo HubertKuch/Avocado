94cae1342d7e8e9ee4ad81edbb4aa09f2df13a13
HubertKuch <kuchhubert@gmail.com>
2023-02-11 15:03:16 +0100

    Get response for tests reasons

--- src/Tests/MockedHttp.php
+++ src/Tests/MockedHttp.php
@@ -2,13 +2,34 @@
 
 namespace Avocado\Tests;
 
+use Avocado\HTTP\ContentType;
 use Avocado\HTTP\HTTPMethod;
+use Avocado\HTTP\HTTPStatus;
+use Avocado\HTTP\ResponseBody;
+use Avocado\Router\HttpResponse;
+use Avocado\Utils\Arrays;
 
 class MockedHttp {
 
     public static function mockPlainRequest(HTTPMethod $method, string $endpoint): void {
         $_SERVER["REQUEST_METHOD"] = $method->value;
         $_SERVER['PHP_SELF'] = $endpoint;
+    }
+
+    public static function getResponse(): ResponseBody {
+        $headers = headers_list();
+        $body = ob_get_contents();
+        $status = http_response_code();
+        $contentType = Arrays::find($headers, fn($header) => str_starts_with("Content-Type: ", $header));
+
+        if ($contentType) {
+            $contentType = str_replace("Content-Type: ", "", $contentType);
+            $contentType = trim($contentType);
+            $contentType = substr($contentType, 0, strpos($contentType, ";"));
+            $contentType = ContentType::from(trim($contentType));
+        }
+
+        return new ResponseBody($body, HTTPStatus::from($status), $contentType);
     }
 
 }
