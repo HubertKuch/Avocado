56c2f1d3cd0a87052b371a44f920823adb6f98a2
HubertKuch <kuchhubert@gmail.com>
2023-03-07 12:18:34 +0100

    Clean up scripts

---
+++ bin/cleanup.php
@@ -0,0 +1,26 @@
+<?php
+
+function deleteDirectory($dir): bool {
+    if (!file_exists($dir)) {
+        return true;
+    }
+
+    if (!is_dir($dir)) {
+        return unlink($dir);
+    }
+
+    foreach (scandir($dir) as $item) {
+        if ($item == '.' || $item == '..') {
+            continue;
+        }
+
+        if (!deleteDirectory($dir . DIRECTORY_SEPARATOR . $item)) {
+            return false;
+        }
+
+    }
+
+    return rmdir($dir);
+}
+
+deleteDirectory(dirname(__DIR__, 1) . "/tests");
